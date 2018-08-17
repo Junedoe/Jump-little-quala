3b3baea9650314fd88347e0f1f8d1cdb9e0f1d59
Ojuna Faust <ojunafaust@MacbookHotspot.local>
2018-08-17 11:39:30 +0200

    images to lowercase

--- js/images.js
+++ js/images.js
@@ -22,6 +22,6 @@ function backImage(imgSrc, x, speed) {
 }
 
 // Background image upper part:
-var backImgUp = new backImage('backImgUp.png', 0, -0.5);
+var backImgUp = new backImage('backimgup.png', 0, -0.5);
 // Background image upper part:
-var backImgBot = new backImage('backImgBott.png', 0, -1);
+var backImgBot = new backImage('backimgbott.png', 0, -1);
